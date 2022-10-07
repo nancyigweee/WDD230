a7c6eb57bfbf558c1c774f5931a988dc5024ee75
nancyigweee <nancyigweee@gmail.com>
2022-10-07 15:21:42 -0600

    next

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//let currentDate = new Date(document.lastModified);
+
 document.getElementById('updatedDate').innerHTML = document.lastModified;
 
 document.getElementById('copyYear').innerHTML = new Date().getFullYear(); // copyright year
