3572e1a22ca618d72cdda89a1803272233629e16
nancyigweee <nancyigweee@gmail.com>
2022-10-07 15:18:02 -0600

    wk 3

---
+++ python-things.py
@@ -0,0 +1,17 @@
+#print("My name is Amarachi
+#name = input("What is your name?")
+#print(name.capitalize())
+
+#school = input("What school do you go to")
+#print (school.capitalize())
+
+
+
+
+
+
+
+#Display three strings “Name”, “Is”, “James” as “Name**Is**James”
+#print(1, 2, 3, 4, sep='*')
+print("name","is","james", sep="**" )
+
